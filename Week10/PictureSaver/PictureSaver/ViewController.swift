f422e46ef07d9201e484e387d4ba01485ade549d
Frederik Søndergaard Jensen <FSJ95@hotmail.dk>
2020-03-06 14:44:15 +0100

    Week10 init

---
+++ Week10/PictureSaver/PictureSaver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureSaver
+//
+//  Created by Frederik Søndergaard Jensen on 06/03/2020.
+//  Copyright © 2020 Frederik Søndergaard Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
