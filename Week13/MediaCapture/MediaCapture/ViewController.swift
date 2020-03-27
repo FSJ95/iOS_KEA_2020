deb96043d45c24a3b1838fb5ac4e6573aa0ce380
Frederik Søndergaard Jensen <FSJ95@hotmail.dk>
2020-03-27 10:19:57 +0100

    Text can be added to image

--- Week13/MediaCapture/MediaCapture/ViewController.swift
+++ Week13/MediaCapture/MediaCapture/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 // ALLOW THE FOLLOWING:
 // Privacy - Photo Library Usage Description
 // Privacy - Photo Library Additions Usage Description
@@ -20,7 +19,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var bottomImageTextField: UITextField!
     @IBOutlet weak var choosePictureButton: UIButton!
+    @IBOutlet weak var addTextButton: UIButton!
     
 //    Will handle fetching the image from the system
     var imagePicker = UIImagePickerController()
@@ -34,9 +35,65 @@ class ViewController: UIViewController {
         showImagePickerNavigation()
     }
     
+    @IBAction func addTextButtonTapped(_ sender: Any) {
+        if let topText = imageTextField.text {
+            if let bottomText = bottomImageTextField.text {
+            if let image = imageView.image {
+                let topImage = textToImage(drawText: topText, inImage: image, atPoint: CGPoint(x: 0, y: 0))
+                let topBottomImage = textToImage(drawText: bottomText, inImage: topImage, atPoint: CGPoint(x: 0, y: topImage.size.height-100))
+                imageView.image = topBottomImage
+            }
+          }
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let p = touches.first?.location(in: view) {
+            imageView.transform = CGAffineTransform(translationX: p.x, y: 0)
+        }
+    }
+    
+    func textToImage(drawText: String, inImage: UIImage, atPoint: CGPoint) -> UIImage{
+
+        // Setup the font specific variables
+        let textColor = UIColor.white;
+        let textFont = UIFont(name: "Helvetica Bold", size: 100)!
+
+        // Setup the image context using the passed image
+        let scale = UIScreen.main.scale
+        UIGraphicsBeginImageContextWithOptions(inImage.size, false, scale)
+
+        // Setup the font attributes that will be later used to dictate how the text should be drawn
+        let textFontAttributes = [
+            NSAttributedString.Key.font: textFont,
+            NSAttributedString.Key.foregroundColor: textColor,
+        ]
+
+        // Put the image into a rectangle as large as the original image
+        inImage.draw(in: CGRect(x: 0, y: 0, width: inImage.size.width, height: inImage.size.height))
+
+        // Create a point within the space that is as bit as the image
+        let rect = CGRect(x: atPoint.x, y: atPoint.y, width: inImage.size.width, height: inImage.size.height)
+
+        // Draw the text into an image
+        drawText.draw(in: rect, withAttributes: textFontAttributes)
+
+        // Create a new image out of the images we have created
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+
+        // End the context now that we have the image we need
+        UIGraphicsEndImageContext()
+        //Pass the image back up to the caller
+        return newImage!
+    }
+    
+    // Remove keyboard when a touch is detected.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        imageTextField.endEditing(true)
+    }
+    
+
 }
-
-
 
 
 //MARK: IMAGE PICKER & NAVIGATION CONTROLLER
